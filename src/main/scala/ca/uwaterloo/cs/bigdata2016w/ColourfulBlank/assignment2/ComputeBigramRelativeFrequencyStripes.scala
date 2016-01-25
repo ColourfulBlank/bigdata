e794e0415c5c6b71bdb21f2b3050ee7d6fe025ad
ColourfulBlank <w257wang@uwaterloo.ca>
2016-01-24 22:01:24 -0500

    Stripes

---
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/ColourfulBlank/assignment2/ComputeBigramRelativeFrequencyStripes.scala
@@ -0,0 +1,72 @@
+package ca.uwaterloo.cs.bigdata2016w.ColourfulBlank.assignment2
+
+import ca.uwaterloo.cs.bigdata2016w.ColourfulBlank.assignment2.Tokenizer
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+
+class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
+  mainOptions = Seq(input, output, reducers)
+  val input = opt[String](descr = "input path", required = true)
+  val output = opt[String](descr = "output path", required = true)
+  val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
+}
+  
+object ComputeBigramRelativeFrequencyPairs extends Tokenizer {
+  val log = Logger.getLogger(getClass().getName())
+  var sum = 0.0f;
+  def tryThis(iter: Iterator[(String, Map[String -> Float])]) : Iterator[(String, Map[String -> Float])] = 
+    {
+      iter.flatMap(curr => {
+        sum = (curr._2)("*")
+        curr._2.keyIterator.map(kit => {
+            (curr._1, Map( (kit, (curr._2)(kit)/sum) ) )
+          })
+          // if (curr._1.lastIndexOf("*") == curr._1.length() - 1) {
+          //   sum = curr._2
+          //   (curr._1, curr._2)
+          // } else {
+          //   (curr._1, curr._2/sum)
+          // }
+        })
+    }
+  def main(argv: Array[String]) {
+    val args = new Conf(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Output: " + args.output())
+    log.info("Number of reducers: " + args.reducers())
+
+    val conf = new SparkConf().setAppName("RF_pairs")
+    val sc = new SparkContext(conf)
+
+    val outputDir = new Path(args.output())
+    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+
+    val textFile = sc.textFile(args.input()) //list of string
+    val counts = textFile.flatMap(line => {
+          val tokens = tokenize(line)
+          if (tokens.length > 1) {
+            tokens.sliding(2).flatMap(p => { 
+              /*val pairStar = List(p.head, "*").mkString(" ")
+              List(pairStar,p.mkString(" ")) */
+              (p.head, Map(("*", 1.0f), (p.tail, 1.0f)))
+              })
+          } else { 
+            // List()
+            ("", Map())
+          }
+        })
+        // .map(bigram => (bigram, 1.0f))
+        .reduceByKey(_ + _)
+        .sortByKey()
+        .mapPartitions(tryThis)
+        .sortByKey()
+
+
+      counts.saveAsTextFile(args.output())
+  }
+}
