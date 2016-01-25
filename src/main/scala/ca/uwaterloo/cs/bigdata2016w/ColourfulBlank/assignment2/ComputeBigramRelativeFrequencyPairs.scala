b88d5c7ae1a0e60f61673202e56b91b0f2301643
w257wang <w257wang@student.cs.uwaterloo.ca>
2016-01-24 20:47:43 -0500

    modified:   src/main/scala/ca/uwaterloo/cs/bigdata2016w/ColourfulBlank/assignment2/ComputeBigramRelativeFrequencyPairs.scala

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/ColourfulBlank/assignment2/ComputeBigramRelativeFrequencyPairs.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/ColourfulBlank/assignment2/ComputeBigramRelativeFrequencyPairs.scala
@@ -14,10 +14,21 @@ class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
   val output = opt[String](descr = "output path", required = true)
   val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
 }
-
-object BigramCount extends Tokenizer {
+  
+object ComputeBigramRelativeFrequencyPairs extends Tokenizer {
   val log = Logger.getLogger(getClass().getName())
-
+  var sum = 0.0f;
+  def tryThis(iter: Iterator[(String, Float)]) : Iterator[(String, Float)] = 
+    {
+      iter.map(curr => {
+          if (curr._1.lastIndexOf("*") == curr._1.length() - 1) {
+            sum = curr._2
+            (curr._1, curr._2)
+          } else {
+            (curr._1, curr._2/sum)
+          }
+        })
+    }
   def main(argv: Array[String]) {
     val args = new Conf(argv)
 
@@ -31,14 +42,25 @@ object BigramCount extends Tokenizer {
     val outputDir = new Path(args.output())
     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
-    val textFile = sc.textFile(args.input())
-    val counts = textFile
-      .flatMap(line => {
-        val tokens = tokenize(line)
-        if (tokens.length > 1) tokens.sliding(2).map(p => p.mkString(" ")).toList else List()
-      })
-      .map(bigram => (bigram, 1))
-      .reduceByKey(_ + _)
-    counts.saveAsTextFile(args.output())
+    val textFile = sc.textFile(args.input()) //list of string
+    val counts = textFile.flatMap(line => {
+          val tokens = tokenize(line)
+          if (tokens.length > 1) {
+            tokens.sliding(2).flatMap(p => { 
+              val pairStar = List(p.head, "*").mkString(" ")
+              List(pairStar,p.mkString(" ")) })
+
+          } else { 
+            List()
+          }
+        })
+        .map(bigram => (bigram, 1.0f))
+        .reduceByKey(_ + _)
+        .sortByKey()
+        .mapPartitions(tryThis)
+        .sortByKey()
+
+
+      counts.saveAsTextFile(args.output())
   }
 }
