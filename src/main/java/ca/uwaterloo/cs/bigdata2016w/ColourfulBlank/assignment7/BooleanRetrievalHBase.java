68017dde9d1e78d79b5928905f39f34aa2aac0c3
ColourfulBlank <w257wang@uwaterloo.ca>
2016-03-30 17:24:56 -0400

    dependency fix

--- src/main/java/ca/uwaterloo/cs/bigdata2016w/ColourfulBlank/assignment7/BooleanRetrievalHBase.java
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/ColourfulBlank/assignment7/BooleanRetrievalHBase.java
@@ -6,7 +6,8 @@ import java.io.InputStreamReader;
 import java.util.Set;
 import java.util.Stack;
 import java.util.TreeSet;
-import java.util.Navigablemap;
+import java.util.NavigableMap;
+import java.util.Map.Entry;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
